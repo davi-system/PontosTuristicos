ac47e4d1414c6095c3093eeff3eab2a1c3a8945d
Davi Alexandre <davialexandre.contato@hotmail.com>
2023-01-13 16:12:48 -0300

    aplicacao criada

---
+++ Projeto/PontosTuristicos/PontosTuristicos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PontosTuristicos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
