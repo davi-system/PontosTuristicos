b6b80c417738484dc11557035430ff6b60d0258e
Davi Alexandre <davialexandre.contato@hotmail.com>
2023-01-19 10:11:11 -0300

    personalizando msg dos campos

--- Projeto/PontosTuristicos/PontosTuristicos/Models/PontoTuristico.cs
+++ Projeto/PontosTuristicos/PontosTuristicos/Models/PontoTuristico.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PontosTuristicos.Models
 {
     public class PontoTuristico
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Informe uma cidade")]
         public string Cidade { get; set; }
-        public int Estado { get; set; }
+        [Required(ErrorMessage = "Informe um estado")]
+        [MaxLength(2)]
+        [MinLength(2, ErrorMessage = "Informe dois dígitos")]
+        public string Estado { get; set; }
+        [Required(ErrorMessage = "Informe um nome")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Informe uma referência")]
         public string Referencia { get; set; }
+        [Required(ErrorMessage = "Informe uma descrição")]
         public string Descricao { get; set; }
         
 
